d09fe9b8c91fbb2faa3ea8789533278cdee42e31
ele <ele@birdlin>
2016-09-24 00:00:09 +0800

    add loop function for read and write into xlsx file

--- test.py
+++ test.py
@@ -3,10 +3,10 @@
 @author: birdlin
 '''
 import openpyxl
-capacity = 0
+mAh = 0
 Wh = 0
 
-wb = openpyxl.load_workbook('rest+discharge.xlsx', read_only=False, )
+wb = openpyxl.load_workbook('rest+discharge.xlsx', read_only=False)
 print (wb.get_sheet_names())
 
 ws = wb.get_sheet_by_name ('sheet1')
@@ -14,36 +14,27 @@ print (ws['B2'].value)
 print (type(ws['B2'].value))
 print (ws.max_row)
 print (ws.max_column)
+row_index = 1
 
 for row in ws.rows:
-    #for cell in row:
-    print (row)
-    print ()
+    if row_index == 1 :
+        ws.cell(row=row_index, column=len(row)+1).value='mV'
+        ws.cell(row=row_index, column=len(row)+2).value='mA'
+        ws.cell(row=row_index, column=len(row)+3).value='mAh'
+        ws.cell(row=row_index, column=len(row)+4).value='Wh'
+        row_index += 1
+        continue
+    Total_Vol = row[0].value * 1000
+    Current_mA = row[1].value * 1000 
+    mAh += Current_mA * 10 /3600
+    Wh += Current_mA/1000 * Total_Vol/1000 * 10 /3600
+    ws.cell(row=row_index, column=len(row)+1).value = Total_Vol
+    ws.cell(row=row_index, column=len(row)+2).value = Current_mA
+    ws.cell(row=row_index, column=len(row)+3).value = mAh
+    ws.cell(row=row_index, column=len(row)+4).value = Wh    
+    row_index += 1
+
+wb.save(filename = 'new_rest+discharge.xlsx')
 
 
-#print (ws.cell(column=2, row=2).value)
 
-
-'''
-with open ('rest+discharge.txt', 'r', encoding='utf-8') as fin:
-    with open ('newtestCSV.txt', 'w', encoding='utf-8') as fout :
-        csvreader = csv.reader(fin, delimiter=',')
-        csvwriter = csv.writer(fout, delimiter=',')
-        header = next(csvreader)
-        header.append('mAh')                        ## V,I,P,R,T,mAH,WH,mAh
-        header.append('Wh')                         ## V,I,P,R,T,mAH,WH,mAh,Wh
-        csvwriter.writerow(header)
-        for row in csvreader:
-            row[0]= round(float(row[0]), 3)         ## Voltage
-            row[1]= round(float(row[1]), 3)         ## Current
-            row[2]= round(float(row[2]), 3)         ## I,P,R,T,mAH,WH
-            row[3]= round(float(row[3]), 3)         ## Resistance
-            row[4]= round(float(row[4]), 3)         ## Temperature
-            row[5]= round(float(row[5]), 3)         ## mAh - LM
-            row[6]= round(float(row[6]), 3)         ## Wh - LM
-            capacity += row[1] * 1000 * 10/3600     ## mAh
-            Wh += row[1] * row[0] * 10/3600         ## Wh
-            row.append(round(capacity, 3))          ## Extend Row
-            row.append(round(Wh, 3))                ## Extend Row
-            csvwriter.writerow(row)
-'''
